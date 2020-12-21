0dfd29ada3e400cc40728c364bb5064b7c4ec889
tehyeexin <73182761+tehyeexin@users.noreply.github.com>
2020-12-21 11:55:39 +0800

    Add files via upload

--- src/Assignment1/Assignment1.java
+++ src/Assignment1/Assignment1.java
@@ -17,10 +17,8 @@ public class Assignment1 {
 		public static void printbackground() {
 			Scanner in = new Scanner (System.in);
 			
-			System.out.println("Personal Background");
-			System.out.println();
+			System.out.println("1. Personal Background");
 			
-			System.out.println("1.");
 			System.out.print("Enter your name: ");
 			String name1 = in.next();
 			System.out.print("Enter your college: ");
@@ -35,43 +33,40 @@ public class Assignment1 {
 		public static void printservice() {
 			Scanner in = new Scanner (System.in);
 			
-			System.out.println("Services Provided");
-			System.out.println("Service No.1: Count Salary Per Month\"");
+			System.out.println("2. Services Provided");
 			
+			System.out.println("Service No.1: Count Salary Per Month");
 			System.out.println("Service No.2: Check Allowance");
-			
 			System.out.println("Service No.3: Sum Total Of The Salary");
-		
+			System.out.println();
 		}
 		
 		public static void printservicecharge() {
 			Scanner in = new Scanner (System.in);
 			
-			System.out.println("Service Charge Per Hour");
+			System.out.println("3. Service Charge Per Hour");
 			System.out.println("RM10 per 1 hour");
-			
+			System.out.println();
 		}
 		
 		public static void printquantityservice() {
 			Scanner in = new Scanner (System.in);
 			
-			System.out.println("Order Services");
-			
-
-				System.out.println("Enter the service number you want to order:");
+			System.out.println("4. Order Services");
+			System.out.print("Enter the service number you want to order: ");
 				int servicenum = in.nextInt();
 				
 				switch(servicenum) {
 				
-				case 1 : // Count Salary Per Month
+				case 1 : 
 					System.out.println("Count Salary Per Month");
 					break;
 					
-				case 2 : // Check Allowance
+				case 2 : 
 					System.out.println("Check Allowance");
 					break;
 					
-				case 3 : // Sum Total Of The Salary
+				case 3 : 
 					System.out.println("Sum Total Of The Salary");
 					break;
 					
@@ -79,12 +74,16 @@ public class Assignment1 {
 					System.out.println("Invalid");
 				}
 				
-				System.out.println("Enter the service hour:");
+				System.out.println();
+				
+				System.out.print("Enter the service hour: ");
 				int hour = in.nextInt();
+				System.out.println();
+				
+				System.out.println("5. Total Price");
+				
 				int total = hour * 10;
-				System.out.println("Total: RM " + total);
-		
-	
+				System.out.print("Total: RM " + total);
 		}
 			
 		
