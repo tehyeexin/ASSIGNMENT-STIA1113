f5c46ee6828180aa75edc3b6402dc935d3b5563d
tehyeexin <73182761+tehyeexin@users.noreply.github.com>
2021-01-07 12:09:17 +0800

    Add files via upload

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -11,6 +11,7 @@ public class Assignment2 {
 		printbackground();
 		printservice();
 		printquantityservice();
+		printconfirmservice();
 		in.close();
 	}
 		public static void printbackground() {
@@ -35,6 +36,7 @@ public class Assignment2 {
 			
 			System.out.println("2. Services Provided");
 			int Totalservice=3;
+
 			
 			for(int i=0;i<=Totalservice;i++) {
 				if(i==1)
@@ -52,26 +54,27 @@ public class Assignment2 {
 				System.out.println("Service No."+ i +": Total Of The Salary - RM15 per hour");
 				}
 			}
-			
-		
 			System.out.println();
 		}
-		
 		
 		public static void printquantityservice() {
 			Scanner in = new Scanner (System.in);
 			
 			System.out.println("3. Order Services");
-			char confirm;
 			int servicenum;
 			int servicehour;
+			char confirm;
+			
 			do {
 				System.out.print("Enter the service number you want to order: ");
 				 servicenum = in.nextInt();
 				System.out.print("Enter the service hour you want to order: ");
 				 servicehour = in.nextInt();
-				System.out.print("Confirm your order(Y/N) ");
+				System.out.print("Confirm your order (Y/N): ");
 				 confirm = in.next().charAt(0);
+				 
+				System.out.println();
+				 
 			} while (confirm != 'Y');
 			
 				
@@ -96,8 +99,27 @@ public class Assignment2 {
 				else {
 					System.out.println("The number you entered is not in the services.");
 				} 
-				
-						}
+			System.out.println();
+		}
 	
-
+		public static void printconfirmservice() {
+			Scanner in = new Scanner (System.in);
+		
+			char confirmPayment;
+			System.out.print("Confirm your order (Y/N): ");
+			confirmPayment = in.next().charAt(0);
+			
+			if(confirmPayment == 'Y') {
+				System.out.println("Please proceed to the counter for payment.");
+			}
+			
+			else {
+				System.out.println("Please exit and re-enter the information.");
+			}
+		
+			
+		
+		
+		}
+	
 }
